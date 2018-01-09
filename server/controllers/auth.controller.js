b6f3e4391eb378b397beebb6e25249346560ec03
Sergei Plahuta <spl@ciklum.com>
2018-01-09 23:45:33 +0200

    add login validation

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -3,11 +3,9 @@ import httpStatus from 'http-status';
 import APIError from '../helpers/APIError';
 import config from '../../config/config';
 
-// sample user, used for authentication
-const user = {
-    username: 'react',
-    password: 'express',
-};
+import db from '../../config/sequelize';
+
+const { User } = db;
 
 /**
  * Returns jwt token if valid username and password is provided
@@ -17,21 +15,22 @@ const user = {
  * @returns {*}
  */
 function login(req, res, next) {
-    // Ideally you'll fetch this from the db
-    // Idea here was to show how jwt works with simplicity
-    if (req.body.username === user.username && req.body.password === user.password) {
-        const token = jwt.sign({
-            username: user.username,
-            expiresIn: 3600,
-        }, config.jwtSecret);
-        return res.json({
-            token,
-            username: user.username,
-        });
-    }
-
-    const err = new APIError('Authentication error', httpStatus.UNAUTHORIZED, true);
-    return next(err);
+    User.findOne({ where: { username: req.body.username } })
+        .then((user) => {
+            if (!user) {
+                const err = new APIError('Authentication error', httpStatus.UNAUTHORIZED, true);
+                return next(err);
+            }
+            const token = jwt.sign({
+                username: user.username,
+                expiresIn: 3600,
+            }, config.jwtSecret);
+            return res.json({
+                token,
+                username: user.username,
+            });
+        })
+        .catch(e => next(e));
 }
 
 /**
--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import expressJwt from 'express-jwt';
 import userRoutes from './user.route';
 import transactionsRoutes from './transaction.route';
 import authRoutes from './auth.route';
+
+import config from '../../config/config';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
@@ -10,7 +13,9 @@ router.get('/health-check', (req, res) =>
     res.send('OK'));
 
 // mount user routes at /users
-router.use('/users', userRoutes);
+router.use('/users', expressJwt({
+    secret: config.jwtSecret,
+}), userRoutes);
 
 // mount user routes at /users
 router.use('/transactions', transactionsRoutes);
