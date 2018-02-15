707ee68efaabc4225580c8b06412425a11db4695
Sergei Plahuta <spl@ciklum.com>
2018-02-15 16:07:44 +0200

    fix Procfile

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-import config from './config/config';
-import app from './config/express';
-/* eslint-disable no-unused-vars */
-import db from './config/sequelize';
+// import config from './config/config';
+// import app from './config/express';
+// /* eslint-disable no-unused-vars */
+// import db from './config/sequelize';
+//
+// const debug = require('debug')('amida-api-boilerplate:index');
+// /* eslint-enable no-unused-vars */
+//
+// // make bluebird default Promise
+// Promise = require('bluebird'); // eslint-disable-line no-global-assign
+//
+// // module.parent check is required to support mocha watch
+// if (!module.parent) {
+//     // listen on port config.port
+//     app.listen(config.port, () => {
+//         console.info(`server started on port ${config.port} (${config.env})`);
+//     });
+// }
+//
+// export default app;
 
-const debug = require('debug')('amida-api-boilerplate:index');
-/* eslint-enable no-unused-vars */
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// make bluebird default Promise
-Promise = require('bluebird'); // eslint-disable-line no-global-assign
+app.get('/', function(request, response) {
+    response.send('Hello from Express!');
+    console.log('Hello')
+})
 
-// module.parent check is required to support mocha watch
-if (!module.parent) {
-    // listen on port config.port
-    app.listen(config.port, () => {
-        console.info(`server started on port ${config.port} (${config.env})`);
-    });
-}
+app.listen(port, function(err) {
+    if(err) {
+        return console.log('something bad happened', err);
+    }
 
-export default app;
+    console.log(`server is listening on ${port}`);
+})
